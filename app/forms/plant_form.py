d0a3c5114329b4ecf815f259433dc522ebe6b144
Ctam312 <ctam312@gmail.com>
2023-02-10 18:41:16 -0500

    plant form completed

--- app/forms/plant_form.py
+++ app/forms/plant_form.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, IntegerField, SubmitField
+from wtforms import StringField, IntegerField, FloatField
 from wtforms.validators import DataRequired
 
 class PlantForm(FlaskForm):
     name = StringField('Name', validators=[DataRequired('Plant must have name.')])
-    price = StringField('Price', validators=[DataRequired('Plant must have name.')])
+    price = FloatField('Price', validators=[DataRequired('Plant must have name.')])
     details = StringField('Details', validators=[DataRequired('Plant must have name.')])
     preview_image_url = StringField('Preview Image URL', validators=[DataRequired('Plant must have name.')])
     user_id = IntegerField('User ID', validators=[DataRequired()])
