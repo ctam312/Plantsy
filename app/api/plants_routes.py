02e5d74a325240a29390348dca32fd6b563a9e92
tiana3113 <t.huynh3113@gmail.com>
2023-02-10 15:56:54 -0800

    imports and first get route

---
+++ app/api/plants_routes.py
@@ -0,0 +1,13 @@
+from flask import Blueprint, jsonify, request, session, redirect
+from flask_login import login_required
+from app.forms.plant_form import PlantForm
+from app.models import Plant, db
+
+plants_routes = Blueprint('plants_routes', __name__)
+
+# GET ROUTEs
+@plants_routes.route("/")
+def plants_home():
+    allPlants = Plant.query.all()
+    return {"allPlants": [plant.to_dict() for plant in allPlants]}
+
