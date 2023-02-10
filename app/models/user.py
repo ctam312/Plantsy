5c722344967a075d7d731c4ca7a2393d365d70e2
lamtran415 <austin415@ymail.com>
2023-02-10 12:33:40 -0800

    Updated Database

--- app/models/user.py
+++ app/models/user.py
@@ -14,6 +14,10 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
 
+    plant = db.relationship("Plant", back_populates="user")
+    review = db.relationship("Review", back_populates="user")
+    shopping_session = db.relationship("ShoppingSession", back_populates="user")
+
     @property
     def password(self):
         return self.hashed_password
