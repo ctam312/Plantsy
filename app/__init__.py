ca3ee2675147590fb11742bf0aad5fe670b616c2
lamtran415 <austin415@ymail.com>
2023-02-13 13:44:20 -0800

    Review Routes, CreateReviewModal, UpdateReviewModal, ReviewCards

--- app/__init__.py
+++ app/__init__.py
@@ -29,7 +29,7 @@ app.cli.add_command(seed_commands)
 app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
 app.register_blueprint(auth_routes, url_prefix='/api/auth')
-app.register_blueprint(review_routes, url_prefix='/api/plants')
+app.register_blueprint(review_routes, url_prefix='/api/reviews')
 db.init_app(app)
 migrate = Migrate(app, db)
 
