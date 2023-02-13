8e2cb68070220375a0a495269f96bd65349705dc
tiana3113 <t.huynh3113@gmail.com>
2023-02-13 09:33:03 -0800

    allplants react

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -5,7 +5,8 @@ import SignupFormPage from "./components/SignupFormPage";
 import LoginFormPage from "./components/LoginFormPage";
 import { authenticate } from "./store/session";
 import Navigation from "./components/Navigation";
-import OnePlant from "./components/OnePlant";
+import OnePlant from "./components/Plants/OnePlant";
+import AllPlants from "./components/Plants/AllPlants";
 
 function App() {
   const dispatch = useDispatch();
@@ -19,13 +20,16 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
-          <Route path="/login" >
+          <Route path="/login">
             <LoginFormPage />
           </Route>
           <Route path="/signup">
             <SignupFormPage />
           </Route>
-          <Route path='/plants/:plantId'>
+          <Route exact path="/">
+            <AllPlants />
+          </Route>
+          <Route path="/plants/:plantId">
             <OnePlant />
           </Route>
         </Switch>
---
+++ react-app/src/components/Plants/AllPlants.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { getAllPlantsThunk } from "../../store/plants";
+
+function AllPlants() {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  useEffect(() => {
+    dispatch(getAllPlantsThunk());
+  }, [dispatch]);
+
+  const allPlants = useSelector((state) => state.plants.allPlants);
+  const allPlantsArr = Object.values(allPlants);
+  if (!allPlantsArr) return null;
+
+  return (
+    <div className="plants-wrapper">
+      <div className="all-plants">
+        {allPlantsArr.map((plant) => (
+          <div
+            key={plant.id}
+            className="plant-card"
+            onClick={() => history.push(`/plants/${plant.id}`)}
+          >
+            <div className="plants-card-wrapper">
+              <img
+                className="plants-image"
+                src={plant.preview_image_url}
+                alt={`NO IMAGE AVAILABLE`}
+              />
+            </div>
+
+            <div className="plant-price">
+              <span className="price">${plant.price}&nbsp;</span>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default AllPlants;
