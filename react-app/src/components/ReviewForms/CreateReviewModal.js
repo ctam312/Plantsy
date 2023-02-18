2569af02dbb51f8a269d7fc7c02ad565543fb288
jared <hem.jared@gmail.com>
2023-02-18 14:28:22 -0800

    more logs to track data

--- react-app/src/components/ReviewForms/CreateReviewModal.js
+++ react-app/src/components/ReviewForms/CreateReviewModal.js
@@ -10,6 +10,8 @@ const CreateReviewModal = () => {
     const history = useHistory()
 	const myPlant = useSelector((state) => state.plants.singlePlant);
 
+    console.log('myplant ----> ', myPlant)
+
     const [review, setReview] = useState("");
     const [stars, setStars] = useState("");
     const [image, setImage] = useState("");
